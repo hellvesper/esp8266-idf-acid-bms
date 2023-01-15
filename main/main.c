dc5119dc7ca9be5a78a13169b4b147f1b7206866
Vespertilio Sh <hellvesper@gmail.com>
2023-01-15 21:04:49 +0200

    cleanup

--- main/main.c
+++ main/main.c
@@ -65,41 +65,6 @@ static const char *TAG = "main";
 #define I2C_BUS_TICKS_TO_WAIT (I2C_BUS_MS_TO_WAIT/portTICK_RATE_MS)
 #define I2C_ACK_CHECK_EN                    0x1     /*!< I2C master will check ack from slave*/
 
-#define MPU6050_SENSOR_ADDR                 ADS111X_ADDR_GND             /*!< slave address for MPU6050 sensor */
-#define MPU6050_CMD_START                   0x41             /*!< Command to set measure mode */
-#define MPU6050_WHO_AM_I                    0x75             /*!< Command to read WHO_AM_I reg */
-#define WRITE_BIT                           I2C_MASTER_WRITE /*!< I2C master write */
-#define READ_BIT                            I2C_MASTER_READ  /*!< I2C master read */
-#define ACK_CHECK_EN                        0x1              /*!< I2C master will check ack from slave*/
-#define ACK_CHECK_DIS                       0x0              /*!< I2C master will not check ack from slave */
-#define ACK_VAL                             0x0              /*!< I2C ack value */
-#define NACK_VAL                            0x1              /*!< I2C nack value */
-#define LAST_NACK_VAL                       0x2              /*!< I2C last_nack value */
-
-/**
- * Define the mpu6050 register address:
- */
-#define SMPLRT_DIV      0x19
-#define CONFIG          0x1A
-#define GYRO_CONFIG     0x1B
-#define ACCEL_CONFIG    0x1C
-#define ACCEL_XOUT_H    0x3B
-#define ACCEL_XOUT_L    0x3C
-#define ACCEL_YOUT_H    0x3D
-#define ACCEL_YOUT_L    0x3E
-#define ACCEL_ZOUT_H    0x3F
-#define ACCEL_ZOUT_L    0x40
-#define TEMP_OUT_H      0x41
-#define TEMP_OUT_L      0x42
-#define GYRO_XOUT_H     0x43
-#define GYRO_XOUT_L     0x44
-#define GYRO_YOUT_H     0x45
-#define GYRO_YOUT_L     0x46
-#define GYRO_ZOUT_H     0x47
-#define GYRO_ZOUT_L     0x48
-#define PWR_MGMT_1      0x6B
-#define WHO_AM_I        0x75  /*!< Command to read WHO_AM_I reg */
-
 /**
  * @brief i2c master initialization
  */
